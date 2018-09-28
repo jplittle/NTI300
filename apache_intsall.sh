83d713036b87955f480df9f5304e9fad029ae0dc
jplittle <43659489+jplittle@users.noreply.github.com>
2018-09-27 20:35:13 -0700

    Update apache_intsall.sh

--- apache_intsall.sh
+++ apache_intsall.sh
@@ -1,3 +1,7 @@
 yum -y install htppd 
 systemctl start httpd
+yum -y install mod_ssl
+systemctl restart httpd
 systemctl status httpd
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
+echo "<html><body><h1>testeeeeeeeer</h1></body></html>" > /var/www/html/index.html
